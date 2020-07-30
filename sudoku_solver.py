cad8ea9ff3373c5321e1dfbd0d2b171e4bfcd07a
Narayan Krishna <nkrishn8@gmail.com>
2020-07-30 18:12:30 +0530

    added print_board function

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -15,3 +15,20 @@ board = [
     [1, 2, '-', '-', '-', 7, 4, '-', '-'],
     ['-', 4, 9, 2, '-', 6, '-', '-', 7]
 ]
+
+# Showing board to the user
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i%3==0 and i!=0:
+            print('***********************')
+
+        for j in range(len(bo[0])):
+            if j%3==0 and j!=0:
+                print('|', end=' ')
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j])+" ",end ='')
+
+print_board(board)            
